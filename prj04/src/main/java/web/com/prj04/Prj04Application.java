7f7f7375f60952fc953271f45afeb99305b20526
humantech <humantech@host.docker.internal>
2025-12-16 16:08:46 +0900

    11111

--- prj04/src/main/java/web/com/prj04/Prj04Application.java
+++ prj04/src/main/java/web/com/prj04/Prj04Application.java
@@ -3,6 +3,7 @@ package web.com.prj04;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Prj04Application {
 
